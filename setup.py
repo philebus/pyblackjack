06e93f9761ced3747ef2f26773c7d0475126397f
Jim <krooskos@gmail.com>
2014-05-07 09:54:48 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pyblackjack',
+    version='1.0',
+    description='Blackjack shoe/deck and validation utilities',
+    packages=['pyblackjack', 'pyblackjack.tests'],
+    test_suite='pyblackjack.tests.test_pyblackjack'
+   
+    )
