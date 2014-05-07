f982795a81dd88c18dc18ce93b53d6c922a62272
Jim <krooskos@gmail.com>
2014-05-07 10:31:00 -0700

    added long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='pyblackjack',
     version='1.0',
-    description='Blackjack shoe/deck and validation utilities',
+    description=('Blackjack shoe/deck and validation utilities'),
+    long_description=read('README.md')
     packages=['pyblackjack', 'pyblackjack.tests'],
+    license='MIT'
     test_suite='pyblackjack.tests.test_pyblackjack'
     )
